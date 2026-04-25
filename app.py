d27019726378d15be75e8182b0bd28f1627c6d1c
Joncy Keda <kedajoncy21@gmail.com>
2026-04-25 09:20:47 +0530

    Add image caption generator using Streamlit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+from utils.caption_generator import generate_caption
+
+st.set_page_config(page_title="Image Caption Generator", layout="wide")
+
+st.title("🖼️ AI Image Caption Generator")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    if st.button("Generate Caption"):
+        caption = generate_caption(image)
+        st.subheader("🧠 Generated Caption")
+        st.write(caption)
