16cd036b1cd0f5a7635c5dcbebb3879f8198cf65
Joncy Keda <kedajoncy21@gmail.com>
2026-04-25 09:21:58 +0530

    Refactor import statement and update file uploader

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PIL import Image
-from utils.caption_generator import generate_caption
+from model.predict import generate_caption
 
 st.set_page_config(page_title="Image Caption Generator", layout="wide")
 
 st.title("🖼️ AI Image Caption Generator")
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file)
